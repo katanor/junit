1df6a9c5c94879397c66e08b780eeaab5cc8c6b4
katanor <krajkumar1266@gmail.com>
2015-03-25 19:28:39 +0530

    first commit by katanor , added a clas.s calle test

---
+++ junit/src/main/java/com/sample/junit/TestDemo.java
@@ -0,0 +1,11 @@
+package com.sample.junit;
+
+public class TestDemo {
+	public static void main(String[] args) {
+		System.out.println(getName());
+	}
+
+	private static String getName() {
+		return "Welcome to git hub";
+	}
+}
